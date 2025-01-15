0c8abcae83c98e6e779e60ce980fcbdb80be4cc5
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-01-15 19:39:52 +0200

    task2

--- js/hw_01.js
+++ js/hw_01.js
@@ -15,12 +15,7 @@ console.log(getShippingMessage("Germany", 80, 20));
 console.log(getShippingMessage("Sweden", 100, 20));
 //task3
 function getElementWidth(content, padding, border) {
-  const contentToNumber = Number.parseFloat(content);
-  const paddingToNumber = Number.parseFloat(padding);
-  const borderToNumber = Number.parseFloat(border);
-
-  const commonWidthElement = contentToNumber + (paddingToNumber + borderToNumber) * 2;
-  return commonWidthElement;
+  return +parseFloat(`${content}`) + (+parseFloat(`${padding}`) + +parseFloat(`${border}`)) * 2;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
